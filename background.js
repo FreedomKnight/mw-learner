7af9544bf713230ec3194e297528f8924196609d
FreedomKnight <j26733514@gmail.com>
2018-02-27 23:58:30 +0800

    dynamic update searching sting

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 contextMenuOption = {
-    title: "Search on Merriam-Webster's Learner",
+    title: "Search '%s' on Merriam-Webster Learner's Dictionary",
     contexts: ["selection"],
     onclick(info) {
         chrome.tabs.create({url: `http://www.learnersdictionary.com/definition/${info.selectionText}`})
     }
 }
 
-chrome.contextMenus.create(contextMenuOption)
+let contextMenu = chrome.contextMenus.create(contextMenuOption)
